40891b71b24356f763d464fafb690e29d83e9f97
George Acosta <georgeacostar@gmail.com>
2020-02-01 15:24:34 -0800

    update product controller and removed location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_210328) do
+ActiveRecord::Schema.define(version: 2020_02_01_041445) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
