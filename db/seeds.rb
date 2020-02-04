0967e4ea4a5c47f672bfc59a37f1db6fd833ecc3
George Acosta <georgeacostar@gmail.com>
2020-02-03 18:49:00 -0800

    add faker gem and seed loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |index|
+    products =[]
+    products.push(Product.create!(name: Faker::Beer.name))
+    products 
+end
